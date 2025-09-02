d9ded8d298348b9a883ede5872b17a2f8934446f
gpdbs9409 <gpdbs9409@naver.com>
2025-09-02 15:06:46 +0900

    update:스키마 검증에 옵셔널 추가

--- mission1_ESM/src/model/branch.ts
+++ mission1_ESM/src/model/branch.ts
@@ -4,11 +4,11 @@ import mongoose from "mongoose";
 //원래는 필드값을 옵셔널로 두지 않았지만, 옵셔널로 두지 않으니 스키마 검증을 하나도 통과하지 못해서 옵셔널로 둠.
 
 const branchschema = new mongoose.Schema({
-  brandName: { type: String, required: false },
-  branchName: { type: String, required: false },
-  address: { type: String, required: false },
+  brandName: { type: String },
+  branchName: { type: String },
+  address: { type: String },
   location: {
-    type: { type: String, required: false },
+    type: { type: String },
     coordinates: { type: [String, String], required: false },
   },
   timestamp: { type: String, required: false },
