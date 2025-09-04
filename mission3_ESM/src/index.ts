c5a3e4df84e01a1654b5104738de4f7563d36049
gpdbs9409 <gpdbs9409@naver.com>
2025-09-04 18:15:49 +0900

    fix:mission3 서버 종료시db disconnection 추가

--- mission3_ESM/src/index.ts
+++ mission3_ESM/src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import franchiseRoutes from "./routes/franchise.routes";
 import swaggerUi from "swagger-ui-express";
 import { createSwaggerSpec } from "./utils/swagger";
-
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 app.use(express.json()); //json 형식으로 데이터를 받기 위한 미들웨어
@@ -18,9 +17,9 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 async function startServer() {
   try {
+    mongoose.connect(process.env.MONGODB_URI as string);
     app.listen(3003, () => {
       console.log("🚀 서버가 시작되었습니다!");
-      mongoose.connect(process.env.MONGODB_URI as string);
       console.log(`swagger UI: http://localhost:3003/api-docs`);
       console.log("connected to mongodb");
     });
@@ -28,6 +27,11 @@ async function startServer() {
     console.error("❌ 서버 시작 실패:", error);
     process.exit(1);
   }
+  process.on("SIGINT", async () => {
+    await mongoose.disconnect();
+    console.log("�� MongoDB 연결 종료");
+    process.exit(0);
+  });
 }
 
 startServer();
